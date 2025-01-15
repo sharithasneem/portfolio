76ed936bbc98d82f9e9b5005bfab3194ed4372c5
sharibathasneemCitriot <shariba.t@citriot.com>
2025-01-15 16:26:08 +0530

    tt commit

--- src/app/component/sliding/page.js
+++ src/app/component/sliding/page.js
@@ -1,29 +1,29 @@
-import { motion, useScroll, useTransform } from "framer-motion";
-import PropTypes from "prop-types";
+// import { motion, useScroll, useTransform } from "framer-motion";
+// import PropTypes from "prop-types";
 
-const SlidingText = ({ text, direction = "left" }) => {
-    const { scrollYProgress } = useScroll();
-    const x = useTransform(
-        scrollYProgress,
-        [0, 1],
-        direction === "left" ? ["100%", "-60%"] : ["-100%", "40%"]
-    );
+// const SlidingText = ({ text, direction = "left" }) => {
+//     const { scrollYProgress } = useScroll();
+//     const x = useTransform(
+//         scrollYProgress,
+//         [0, 1],
+//         direction === "left" ? ["100%", "-60%"] : ["-100%", "40%"]
+//     );
 
-    return (
-        <div className="overflow-hidden w-full">
-            <motion.div
-                className="text-7xl font-bold text-white whitespace-nowrap font-mono"
-                style={{ x }}
-            >
-                {text}
-            </motion.div>
-        </div>
-    );
-};
+//     return (
+//         <div className="overflow-hidden w-full">
+//             <motion.div
+//                 className="text-7xl font-bold text-white whitespace-nowrap font-mono"
+//                 style={{ x }}
+//             >
+//                 {text}
+//             </motion.div>
+//         </div>
+//     );
+// };
 
-SlidingText.propTypes = {
-    text: PropTypes.string.isRequired,
-    direction: PropTypes.oneOf(["left", "right"]),
-};
+// SlidingText.propTypes = {
+//     text: PropTypes.string.isRequired,
+//     direction: PropTypes.oneOf(["left", "right"]),
+// };
 
-export default SlidingText;
+// export default SlidingText;
